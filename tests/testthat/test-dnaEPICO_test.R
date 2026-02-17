11f235726823db3dd17efe1c8955d37e54a7d106
Paul Ruiz <ruizpint@qut.edu.au>
2026-02-17 13:08:27 +1000

    Codecov 0.99.0

--- tests/testthat/test-dnaEPICO_test.R
+++ tests/testthat/test-dnaEPICO_test.R
@@ -61,6 +61,8 @@ test_that("preprocessingMinfiEwasWater runs using staged minfiData IDATs", {
     targets$Sentrix_Position
   )
 
+  targets$Timepoint <- 1
+
   phenoFile <- file.path(tmpDir, "pheno_minfiData.csv")
 
   write.csv(
@@ -104,9 +106,125 @@ test_that("preprocessingMinfiEwasWater runs using staged minfiData IDATs", {
   )
 
   # ------------------------------------------------------------------
-  # 7. Minimal output sanity checks
+  # 7. Run sva
   # ------------------------------------------------------------------
 
-  expect_true(dir.exists(file.path(tmpDir, "logs")))
-  expect_true(dir.exists(file.path(tmpDir, "rData")))
+  rgsetPath <- file.path(
+    tmpDir,
+    "rData",
+    "preprocessingMinfiEwasWater",
+    "objects",
+    "RGSet.RData"
+  )
+
+  expect_true(file.exists(rgsetPath))
+
+  expect_error(
+      dnaEPICO::svaEnmix(
+        phenoFile = phenoFile,
+        rgsetData = rgsetPath,
+        sepType = "",
+        outputLogs = file.path(tmpDir, "logs"),
+        nSamples = NA,
+        SampleID = "Sample_Name",
+        arrayType = "IlluminaHumanMethylation450k",
+        annotationVersion = "ilmn12.hg19",
+        SentrixIDColumn = "Sentrix_ID",
+        SentrixPositionColumn = "Sentrix_Position",
+        ctrlSvaPercVar = 0.90,
+        ctrlSvaFlag = 1,
+        scriptLabel = "svaEnmix",
+        tiffWidth = 2000,
+        tiffHeight = 1000,
+        tiffRes = 150,
+        dataBaseDir = file.path(tmpDir, "data") ,
+        figureBaseDir = file.path(tmpDir, "figures")
+        
+      ),
+      NA
+    )
+
+  # ------------------------------------------------------------------
+  # 8. Run preprocessing phenotype files
+  # ------------------------------------------------------------------
+
+    betaPath <- file.path(
+      tmpDir,
+      "rData",
+      "preprocessingMinfiEwasWater",
+      "metrics",
+      "beta_NomFilt_MSetF_Flt_Rxy_Ds_Rc.RData"
+    )
+
+    mPath <- file.path(
+      tmpDir,
+      "rData",
+      "preprocessingMinfiEwasWater",
+      "metrics",
+      "m_NomFilt_MSetF_Flt_Rxy_Ds_Rc.RData"
+    )
+
+    cnPath <- file.path(
+      tmpDir,
+      "rData",
+      "preprocessingMinfiEwasWater",
+      "metrics",
+      "cn_NomFilt_MSetF_Flt_Rxy_Ds_Rc.RData"
+    )
+
+    expect_true(file.exists(betaPath))
+    expect_true(file.exists(mPath))
+    expect_true(file.exists(cnPath))
+
+    expect_error(
+        dnaEPICO::preprocessingPheno(
+          phenoFile = phenoFile,
+          sepType = "",
+          betaPath = betaPath,
+          mPath = mPath,
+          cnPath = cnPath,
+          SampleID = "Sample_Name",
+          timeVar = "Timepoint",
+          timepoints = "1",
+          combineTimepoints = "1",
+          sexColumn = "sex",
+          outputPheno = file.path(tmpDir, "data", "preprocessingPheno"),
+          outputRData = file.path(tmpDir, "rData", "preprocessingPheno", "metrics"),
+          outputRDataMerge = file.path(tmpDir, "rData", "preprocessingPheno", "mergeData"),
+          outputLogs = file.path(tmpDir, "logs"),
+          outputDir = file.path(tmpDir, "data", "preprocessingPheno")
+        ),
+        NA
+      )
+
+  # ------------------------------------------------------------------
+  # 8. Generate Report
+  # ------------------------------------------------------------------
+    dnamReport(
+      output = "DNAm_Report.pdf",
+      outputDir = file.path(tmpDir, "reports"),
+
+      qcDir = file.path(
+        tmpDir, "figures", "preprocessingMinfiEwasWater", "enMix"
+      ),
+      preprocessingDir = file.path(
+        tmpDir, "figures", "preprocessingMinfiEwasWater", "qc"
+      ),
+      postprocessingDir = file.path(
+        tmpDir, "figures", "preprocessingMinfiEwasWater", "metrics"
+      ),
+      svaDir = file.path(
+        tmpDir, "figures", "svaEnmix"
+      ),
+      glmDir = file.path(
+        tmpDir, "figures", "methylationGLM_T1"
+      ),
+      glmmDir = file.path(
+        tmpDir, "figures", "methylationGLMM_T1T2"
+      ),
+
+      reportTitle = "DNA methylation",
+      author = "School of Biomedical Sciences",
+      date = format(Sys.Date(), "%B %d, %Y")
+    )
 })
