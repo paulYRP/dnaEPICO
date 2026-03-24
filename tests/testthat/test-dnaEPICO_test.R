be5c3b53bbfa7418bc522faaa131437ad9c9ee24
Paul Ruiz <ruizpint@qut.edu.au>
2026-03-24 14:08:32 +1000

    Bioconductor full test version 0.99.6

--- tests/testthat/test-dnaEPICO_test.R
+++ tests/testthat/test-dnaEPICO_test.R
@@ -82,229 +82,229 @@ test_that("preprocessingMinfiEwasWater runs using staged minfiData IDATs", {
   setwd(tmpDir)
   on.exit(setwd(oldWd), add = TRUE)
 
-  # # ------------------------------------------------------------------
-  # # 6. Run preprocessing pipeline
-  # # ------------------------------------------------------------------
-  #
-  # expect_error(
-  #   dnaEPICO::preprocessingMinfiEwasWater(
-  #     phenoFile = phenoFile,
-  #     idatFolder = tmpIdatDir,
-  #     nSamples = 6,
-  #     SampleID = "Sample_Name",
-  #     phenoOrder = "Sample_Name;sex;Basename;Sentrix_ID;Sentrix_Position",
-  #     arrayType = "IlluminaHumanMethylation450k",
-  #     annotationVersion = "ilmn12.hg19",
-  #     sexColumn = "sex",
-  #     plotGroupVar = "sex",
-  #     lcRef = "FlowSorted.Blood.450k",
-  #     outputLogs = file.path(tmpDir, "logs"),
-  #     baseDataFolder = file.path(tmpDir, "rData"),
-  #     crossReactivePath = crossReactivePath,
-  #     lcPhenoDir = tmpDir
-  #   ),
-  #   NA
-  # )
+  # ------------------------------------------------------------------
+  # 6. Run preprocessing pipeline
+  # ------------------------------------------------------------------
 
-  # # ------------------------------------------------------------------
-  # # 7. Run sva
-  # # ------------------------------------------------------------------
-  #
-  # rgsetPath <- file.path(
-  #   tmpDir,
-  #   "rData",
-  #   "preprocessingMinfiEwasWater",
-  #   "objects",
-  #   "RGSet.RData"
-  # )
-  #
-  # expect_true(file.exists(rgsetPath))
-  #
-  # expect_error(
-  #     dnaEPICO::svaEnmix(
-  #       phenoFile = phenoFile,
-  #       rgsetData = rgsetPath,
-  #       sepType = "",
-  #       outputLogs = file.path(tmpDir, "logs"),
-  #       nSamples = NA,
-  #       SampleID = "Sample_Name",
-  #       arrayType = "IlluminaHumanMethylation450k",
-  #       annotationVersion = "ilmn12.hg19",
-  #       SentrixIDColumn = "Sentrix_ID",
-  #       SentrixPositionColumn = "Sentrix_Position",
-  #       ctrlSvaPercVar = 0.90,
-  #       ctrlSvaFlag = 1,
-  #       scriptLabel = "svaEnmix",
-  #       tiffWidth = 2000,
-  #       tiffHeight = 1000,
-  #       tiffRes = 150,
-  #       dataBaseDir = file.path(tmpDir, "data") ,
-  #       figureBaseDir = file.path(tmpDir, "figures"),
-  #       rBaseDir = file.path(tmpDir, "rData")
-  #
-  #     ),
-  #     NA
-  #   )
-  #
-  # # ------------------------------------------------------------------
-  # # 8. Run preprocessing phenotype files
-  # # ------------------------------------------------------------------
-  #
-  #   betaPath <- file.path(
-  #     tmpDir,
-  #     "rData",
-  #     "preprocessingMinfiEwasWater",
-  #     "metrics",
-  #     "beta_NomFilt_MSetF_Flt_Rxy_Ds_Rc.RData"
-  #   )
-  #
-  #   mPath <- file.path(
-  #     tmpDir,
-  #     "rData",
-  #     "preprocessingMinfiEwasWater",
-  #     "metrics",
-  #     "m_NomFilt_MSetF_Flt_Rxy_Ds_Rc.RData"
-  #   )
-  #
-  #   cnPath <- file.path(
-  #     tmpDir,
-  #     "rData",
-  #     "preprocessingMinfiEwasWater",
-  #     "metrics",
-  #     "cn_NomFilt_MSetF_Flt_Rxy_Ds_Rc.RData"
-  #   )
-  #
-  #   expect_true(file.exists(betaPath))
-  #   expect_true(file.exists(mPath))
-  #   expect_true(file.exists(cnPath))
-  #
-  #   expect_error(
-  #       dnaEPICO::preprocessingPheno(
-  #         phenoFile = phenoFile,
-  #         sepType = "",
-  #         betaPath = betaPath,
-  #         mPath = mPath,
-  #         cnPath = cnPath,
-  #         SampleID = "Sample_Name",
-  #         timeVar = "Timepoint",
-  #         timepoints = "1",
-  #         combineTimepoints = "1",
-  #         sexColumn = "sex",
-  #         outputPheno = file.path(tmpDir, "data", "preprocessingPheno"),
-  #         outputRData = file.path(tmpDir, "rData", "preprocessingPheno", "metrics"),
-  #         outputRDataMerge = file.path(tmpDir, "rData", "preprocessingPheno", "mergeData"),
-  #         outputLogs = file.path(tmpDir, "logs"),
-  #         outputDir = file.path(tmpDir, "data", "preprocessingPheno")
-  #       ),
-  #       NA
-  #     )
+  expect_error(
+    dnaEPICO::preprocessingMinfiEwasWater(
+      phenoFile = phenoFile,
+      idatFolder = tmpIdatDir,
+      nSamples = 6,
+      SampleID = "Sample_Name",
+      phenoOrder = "Sample_Name;sex;Basename;Sentrix_ID;Sentrix_Position",
+      arrayType = "IlluminaHumanMethylation450k",
+      annotationVersion = "ilmn12.hg19",
+      sexColumn = "sex",
+      plotGroupVar = "sex",
+      lcRef = "FlowSorted.Blood.450k",
+      outputLogs = file.path(tmpDir, "logs"),
+      baseDataFolder = file.path(tmpDir, "rData"),
+      crossReactivePath = crossReactivePath,
+      lcPhenoDir = tmpDir
+    ),
+    NA
+  )
 
-  # # ------------------------------------------------------------------
-  # # 9. Generate Report
-  # # ------------------------------------------------------------------
-  #
-  # expect_error(
-  #     dnaEPICO::dnamReport(
-  #       output = "DNAm_Report.pdf",
-  #       outputDir = file.path(tmpDir, "reports"),
-  #       qcDir = file.path(
-  #         tmpDir, "figures", "preprocessingMinfiEwasWater", "enMix"
-  #       ),
-  #       preprocessingDir = file.path(
-  #         tmpDir, "figures", "preprocessingMinfiEwasWater", "qc"
-  #       ),
-  #       postprocessingDir = file.path(
-  #         tmpDir, "figures", "preprocessingMinfiEwasWater", "metrics"
-  #       ),
-  #       svaDir = file.path(
-  #         tmpDir, "figures", "svaEnmix"
-  #       ),
-  #       glmDir = file.path(
-  #         tmpDir, "figures", "methylationGLM_T1"
-  #       ),
-  #       glmmDir = file.path(
-  #         tmpDir, "figures", "methylationGLMM_T1T2"
-  #       ),
-  #       figDir = file.path(
-  #         tmpDir, "reports", "figures"
-  #       ),
-  #       reportTitle = "DNA methylation",
-  #       author = "School of Biomedical Sciences",
-  #       date = format(Sys.Date(), "%B %d, %Y")
-  #     ),
-  #       NA
-  #     )
+  # ------------------------------------------------------------------
+  # 7. Run sva
+  # ------------------------------------------------------------------
+
+  rgsetPath <- file.path(
+    tmpDir,
+    "rData",
+    "preprocessingMinfiEwasWater",
+    "objects",
+    "RGSet.RData"
+  )
+
+  expect_true(file.exists(rgsetPath))
+
+  expect_error(
+      dnaEPICO::svaEnmix(
+        phenoFile = phenoFile,
+        rgsetData = rgsetPath,
+        sepType = "",
+        outputLogs = file.path(tmpDir, "logs"),
+        nSamples = NA,
+        SampleID = "Sample_Name",
+        arrayType = "IlluminaHumanMethylation450k",
+        annotationVersion = "ilmn12.hg19",
+        SentrixIDColumn = "Sentrix_ID",
+        SentrixPositionColumn = "Sentrix_Position",
+        ctrlSvaPercVar = 0.90,
+        ctrlSvaFlag = 1,
+        scriptLabel = "svaEnmix",
+        tiffWidth = 2000,
+        tiffHeight = 1000,
+        tiffRes = 150,
+        dataBaseDir = file.path(tmpDir, "data") ,
+        figureBaseDir = file.path(tmpDir, "figures"),
+        rBaseDir = file.path(tmpDir, "rData")
+
+      ),
+      NA
+    )
+
+  # ------------------------------------------------------------------
+  # 8. Run preprocessing phenotype files
+  # ------------------------------------------------------------------
+
+    betaPath <- file.path(
+      tmpDir,
+      "rData",
+      "preprocessingMinfiEwasWater",
+      "metrics",
+      "beta_NomFilt_MSetF_Flt_Rxy_Ds_Rc.RData"
+    )
+
+    mPath <- file.path(
+      tmpDir,
+      "rData",
+      "preprocessingMinfiEwasWater",
+      "metrics",
+      "m_NomFilt_MSetF_Flt_Rxy_Ds_Rc.RData"
+    )
+
+    cnPath <- file.path(
+      tmpDir,
+      "rData",
+      "preprocessingMinfiEwasWater",
+      "metrics",
+      "cn_NomFilt_MSetF_Flt_Rxy_Ds_Rc.RData"
+    )
+
+    expect_true(file.exists(betaPath))
+    expect_true(file.exists(mPath))
+    expect_true(file.exists(cnPath))
+
+    expect_error(
+        dnaEPICO::preprocessingPheno(
+          phenoFile = phenoFile,
+          sepType = "",
+          betaPath = betaPath,
+          mPath = mPath,
+          cnPath = cnPath,
+          SampleID = "Sample_Name",
+          timeVar = "Timepoint",
+          timepoints = "1",
+          combineTimepoints = "1",
+          sexColumn = "sex",
+          outputPheno = file.path(tmpDir, "data", "preprocessingPheno"),
+          outputRData = file.path(tmpDir, "rData", "preprocessingPheno", "metrics"),
+          outputRDataMerge = file.path(tmpDir, "rData", "preprocessingPheno", "mergeData"),
+          outputLogs = file.path(tmpDir, "logs"),
+          outputDir = file.path(tmpDir, "data", "preprocessingPheno")
+        ),
+        NA
+      )
+
+  # ------------------------------------------------------------------
+  # 9. Generate Report
+  # ------------------------------------------------------------------
+
+  expect_error(
+      dnaEPICO::dnamReport(
+        output = "DNAm_Report.pdf",
+        outputDir = file.path(tmpDir, "reports"),
+        qcDir = file.path(
+          tmpDir, "figures", "preprocessingMinfiEwasWater", "enMix"
+        ),
+        preprocessingDir = file.path(
+          tmpDir, "figures", "preprocessingMinfiEwasWater", "qc"
+        ),
+        postprocessingDir = file.path(
+          tmpDir, "figures", "preprocessingMinfiEwasWater", "metrics"
+        ),
+        svaDir = file.path(
+          tmpDir, "figures", "svaEnmix"
+        ),
+        glmDir = file.path(
+          tmpDir, "figures", "methylationGLM_T1"
+        ),
+        glmmDir = file.path(
+          tmpDir, "figures", "methylationGLMM_T1T2"
+        ),
+        figDir = file.path(
+          tmpDir, "reports", "figures"
+        ),
+        reportTitle = "DNA methylation",
+        author = "School of Biomedical Sciences",
+        date = format(Sys.Date(), "%B %d, %Y")
+      ),
+        NA
+      )
 
   # ------------------------------------------------------------------
   # 10. Extract Makefile
   # ------------------------------------------------------------------
-  # expect_error(
-  #     dnaEPICO::extractMake(
-  #       destDir = tmpDir,
-  #       overwrite = TRUE
-  #     ),
-  #       NA
-  #     )
+  expect_error(
+      dnaEPICO::extractMake(
+        destDir = tmpDir,
+        overwrite = TRUE
+      ),
+        NA
+      )
 
-  # # ------------------------------------------------------------------
-  # # 11. Run GLM
-  # # ------------------------------------------------------------------
-  #
-  # expect_error(
-  #     dnaEPICO::methylationGLM_T1(
-  #       inputPheno = file.path(tmpDir, "rData", "preprocessingPheno", "mergeData",
-  #                              "phenoBetaT1.RData"),
-  #       outputLogs = file.path(tmpDir, "logs"),
-  #       outputRData = file.path(tmpDir, "rData", "methylationGLM_T1", "models"),
-  #       outputPlots = file.path(tmpDir, "figures", "methylationGLM_T1"),
-  #       phenotypes = "status",
-  #       covariates = "sex",
-  #       factorVars = "sex,status",
-  #       cpgLimit = 5,
-  #       nCores = 1,
-  #       summaryPval = 1,
-  #       significantCpGDir = file.path(tmpDir, "results", "cpgs", "methylationGLM_T1"),
-  #       summaryTxtDir = file.path(tmpDir, "results", "summary", "methylationGLM_T1",
-  #                                 "glm"),
-  #       annotationCols = "Name,chr,pos,UCSC_RefGene_Group,UCSC_RefGene_Name,Relation_to_Island",
-  #       annotatedGLMOut = file.path(tmpDir, "data", "methylationGLM_T1"),
-  #       annotationPackage = "IlluminaHumanMethylation450kanno.ilmn12.hg19",
-  #       chunkSize = 1,
-  #       significantCpGPval = 1
-  #     ),
-  #       NA
-  #     )
+  # ------------------------------------------------------------------
+  # 11. Run GLM
+  # ------------------------------------------------------------------
 
-  # # ------------------------------------------------------------------
-  # # 12. Run LMER
-  # # ------------------------------------------------------------------
-  #
-  # expect_error(
-  #     dnaEPICO::methylationGLMM_T1T2(
-  #       inputPheno = file.path(tmpDir, "rData", "preprocessingPheno", "mergeData", "phenoBetaT1.RData"),
-  #       outputLogs = file.path(tmpDir, "logs"),
-  #       outputRData = file.path(tmpDir, "rData", "methylationGLMM_T1T2", "models"),
-  #       outputPlots = file.path(tmpDir, "figures", "methylationGLMM_T1T2"),
-  #       phenotypes = "status",
-  #       personVar = "person",
-  #       covariates = "sex",
-  #       factorVars = "sex,status",
-  #       cpgLimit = 10,
-  #       nCores = 1,
-  #       summaryPval = 1,
-  #       significantInteractionDir = file.path(tmpDir, "results", "cpgs", "methylationGLMM_T1T2"),
-  #       summaryTxtDir = file.path(tmpDir, "results", "summary", "methylationGLMM_T1T2", "lmer"),
-  #       annotatedLMEOut = file.path(tmpDir, "data", "methylationGLMM_T1T2"),
-  #       annotationPackage = "IlluminaHumanMethylation450kanno.ilmn12.hg19",
-  #       chunkSize = 1,
-  #       significantInteractionPval = 1,
-  #       saveSignificantInteractions = FALSE,
-  #       annotationCols = "Name,chr,pos,UCSC_RefGene_Group,UCSC_RefGene_Name,Relation_to_Island"
-  #
-  #     ),
-  #       NA
-  #     )
-  #
+  expect_error(
+      dnaEPICO::methylationGLM_T1(
+        inputPheno = file.path(tmpDir, "rData", "preprocessingPheno", "mergeData",
+                               "phenoBetaT1.RData"),
+        outputLogs = file.path(tmpDir, "logs"),
+        outputRData = file.path(tmpDir, "rData", "methylationGLM_T1", "models"),
+        outputPlots = file.path(tmpDir, "figures", "methylationGLM_T1"),
+        phenotypes = "status",
+        covariates = "sex",
+        factorVars = "sex,status",
+        cpgLimit = 5,
+        nCores = 8,
+        summaryPval = 1,
+        significantCpGDir = file.path(tmpDir, "results", "cpgs", "methylationGLM_T1"),
+        summaryTxtDir = file.path(tmpDir, "results", "summary", "methylationGLM_T1",
+                                  "glm"),
+        annotationCols = "Name,chr,pos,UCSC_RefGene_Group,UCSC_RefGene_Name,Relation_to_Island",
+        annotatedGLMOut = file.path(tmpDir, "data", "methylationGLM_T1"),
+        annotationPackage = "IlluminaHumanMethylation450kanno.ilmn12.hg19",
+        chunkSize = 1,
+        significantCpGPval = 1
+      ),
+        NA
+      )
+
+  # ------------------------------------------------------------------
+  # 12. Run LMER
+  # ------------------------------------------------------------------
+
+  expect_error(
+      dnaEPICO::methylationGLMM_T1T2(
+        inputPheno = file.path(tmpDir, "rData", "preprocessingPheno", "mergeData", "phenoBetaT1.RData"),
+        outputLogs = file.path(tmpDir, "logs"),
+        outputRData = file.path(tmpDir, "rData", "methylationGLMM_T1T2", "models"),
+        outputPlots = file.path(tmpDir, "figures", "methylationGLMM_T1T2"),
+        phenotypes = "status",
+        personVar = "person",
+        covariates = "sex",
+        factorVars = "sex,status",
+        cpgLimit = 10,
+        nCores = 8,
+        summaryPval = 1,
+        significantInteractionDir = file.path(tmpDir, "results", "cpgs", "methylationGLMM_T1T2"),
+        summaryTxtDir = file.path(tmpDir, "results", "summary", "methylationGLMM_T1T2", "lmer"),
+        annotatedLMEOut = file.path(tmpDir, "data", "methylationGLMM_T1T2"),
+        annotationPackage = "IlluminaHumanMethylation450kanno.ilmn12.hg19",
+        chunkSize = 1,
+        significantInteractionPval = 1,
+        saveSignificantInteractions = FALSE,
+        annotationCols = "Name,chr,pos,UCSC_RefGene_Group,UCSC_RefGene_Name,Relation_to_Island"
+
+      ),
+        NA
+      )
+
 
 
 })
